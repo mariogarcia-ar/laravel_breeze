66f8c693b1a6e2f1d3c6036fe6e6ac7b9149b0e7
Mario Garcia <mariogarcia.ar@gmail.com>
2023-03-26 15:39:02 +0000

    First commit

---
+++ run_breeze.sh
@@ -0,0 +1,9 @@
+laravel new project
+editar .env
+php artisan optimize:clear &&	php artisan config:clear
+php artisan db:wipe && 	php artisan migrate:refresh --seed
+
+git status 
+git init 
+git add -A
+git commit -m "First commit"
