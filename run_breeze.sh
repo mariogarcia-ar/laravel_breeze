66be9b7dd9722062dfc9c337e07d6d2a74b228f9
Mario Garcia <mariogarcia.ar@gmail.com>
2023-03-26 15:43:08 +0000

    Breeze installed

--- run_breeze.sh
+++ run_breeze.sh
@@ -7,3 +7,11 @@ git status
 git init 
 git add -A
 git commit -m "First commit"
+
+composer require laravel/breeze 
+php artisan breeze:install blade
+
+php artisan db:wipe && 	php artisan migrate:refresh --seed
+php artisan optimize:clear &&php artisan config:clear
+
+git add -A && git commit -m "Breeze installed"
